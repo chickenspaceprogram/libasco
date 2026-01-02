7e8be734b705a73437b95f3b3038636f6a3e93f5
Athena Boose <pestpestthechicken@yahoo.com>
2026-01-02 05:39:52 -0800

    Fixed bugs, added tests

--- src/aarch64.s
+++ src/aarch64.s
@@ -8,31 +8,31 @@
 ; i haven't written much aarch64, so expect inane comments
 
 
-.global asco_init_internal
+.global _asco_init_internal
 ; void asco_init_internal(asco_ctx *new_ctx, asco_fn fn, void *arg, void *sp);
 
-.global asco_save
+.global _asco_save
 ; int asco_save(asco_ctx *cur_ctx);
 
-.global asco_load
+.global _asco_load
 ; void asco_load(const asco_ctx *new_ctx);
 
 
-asco_init_internal:
+_asco_init_internal:
 	; no previous frame, no previous link, so set those to 0
 	stp	xzr, xzr, [x0], #16
 	stp	x3, x1, [x0], #16
 
 	; saving first argument to "x19 reg"
-	str	x2, [x0, #8]
+	str	x2, [x0]
 	
 	ret
 
 
-asco_save:
+_asco_save:
 	; copying sp and intended pc
 	mov	x9, sp
-	ldr	x10, =asco_save_comeback
+	adr	x10, Lasco_save_cameback
 
 	; storing fp, lr, sp, pc
 	stp	fp, lr, [x0], #16
@@ -55,11 +55,11 @@ asco_save:
 	mov	w0, #0
 	ret
 
-asco_save_cameback:
+Lasco_save_cameback:
 	mov	w0, #1
 	ret
 
-asco_load:
+_asco_load:
 	; loading lr, sp, fp, pc
 	ldp	fp, lr, [x0], #16
 	ldp	x9, x10, [x0], #16
@@ -74,19 +74,18 @@ asco_load:
 	ldp	x25, x26, [x0], #16
 	ldp	x27, x28, [x0], #16
 
+	; loading d8, d15
+	ldp	d8, d9, [x0], #16
+	ldp	d10, d11, [x0], #16
+	ldp	d12, d13, [x0], #16
+	ldp	d14, d15, [x0]
+
 	; dirty trick
 	; when starting a new ctx from a function, i've stored its `void *`
 	; arg in the x19 slot. in that case x19 doesn't matter anyways.
 	; when resuming a ctx, x0 is not preserved, so this doesnt cause
 	; issues then either
 	mov	x0, x19
-
-
-	; loading d8, d15
-	ldp	d8, d9, [x0], #16
-	ldp	d10, d11, [x0], #16
-	ldp	d12, d13, [x0], #16
-	ldp	d14, d15, [x0]
 
 	; branching to pc
 	br	x10
--- tests/test-asco.c
+++ tests/test-asco.c
@@ -4,7 +4,60 @@
 
 // SPDX-License-Identifier: MPL-2.0
 
+// these tests suck. but i'm not gonna bother writing platform-specific
+// assembly for tests of all things
+//
+// so, uhhh, if you find bugs, report them lmao
+
+
+#include <asco/asco.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define dbgassert(ARG) do {\
+	if (!(ARG)) {\
+		fprintf(stderr, "%s:%d: %s: Assertion `%s' failed.\n",\
+			__FILE__, __LINE__, __func__, #ARG);\
+		fflush(stderr);\
+		abort();\
+	}\
+} while (0)
+
+
+static void chk_revert(void)
+{
+	asco_ctx ctx;
+	int val = 123;
+	int niter = 6969;
+	if (val++ == 123 && asco_save(&ctx)) {
+		asco_load(&ctx);
+		niter = 1;
+	}
+	dbgassert(niter == 6969);
+}
+
+static void jumpback(void *arg)
+{
+	volatile int foo = 3;
+	dbgassert(foo == 3);
+	asco_ctx *main_ctx = arg;
+	asco_load(main_ctx);
+}
+
+static void chk_switch_stacks(void)
+{
+	int val = 123;
+	asco_ctx main_ctx;
+	asco_ctx new_ctx;
+	void *stack = malloc(0x1000);
+	asco_init(&new_ctx, jumpback, &main_ctx, stack, 0x1000);
+	asco_swap(&main_ctx, &new_ctx);
+	dbgassert(val == 123);
+	free(stack);
+}
 
 int main(void)
 {
+	chk_revert();
+	chk_switch_stacks();
 }
