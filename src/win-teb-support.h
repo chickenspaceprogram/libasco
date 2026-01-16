5901589acad381373c9c90530dcd5fe137e6004d
Athena Boose <pestpestthechicken@yahoo.com>
2026-01-16 03:13:16 -0800

    windows....

--- src/win-teb-support.h
+++ src/win-teb-support.h
@@ -10,19 +10,19 @@
 static void set_teb(asco_ctx *ctx)
 {
 	PTEB teb = NtCurrentTeb();
-	teb->Reserved1[1] = ctx->tib_stack_base;
-	teb->Reserved1[2] = ctx->tib_stack_limit;
+	teb->Reserved1[1] = (void *)ctx->tib_stack_base;
+	teb->Reserved1[2] = (void *)ctx->tib_stack_limit;
 	memcpy(teb->Reserved3 + 1944, &ctx->tib_dealloc_stack, 8);
-	teb->Reserved5[24] = ctx->tib_guaranteed_bytes;
+	teb->Reserved5[24] = (void *)ctx->tib_guaranteed_bytes;
 }
 
 static void get_teb(asco_ctx *ctx)
 {
 	PTEB teb = NtCurrentTeb();
-	ctx->tib_stack_base = teb->Reserved1[1];
-	ctx->tib_stack_limit = teb->Reserved1[2];
+	ctx->tib_stack_base = (uint64_t)teb->Reserved1[1];
+	ctx->tib_stack_limit = (uint64_t)teb->Reserved1[2];
 	memcpy(&ctx->tib_dealloc_stack, teb->Reserved3 + 1944, 8);
-	ctx->tib_guaranteed_bytes = teb->Reserved5[24];
+	ctx->tib_guaranteed_bytes = (uint64_t)teb->Reserved5[24];
 }
 
 
